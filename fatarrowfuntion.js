c6e5d908122d111921da329ddca343900c36d09c
Deepak Suthar <deepak040599@gmail.com>
2021-10-17 16:36:19 +0530

    7563

---
+++ fatarrowfuntion.js
@@ -0,0 +1,17 @@
+// function defination
+  function myFunction(s){  //s is a formal argument 
+    //   function body
+      console.log("myself harsh reality of your cureent scenrio " + s);
+      return s; 
+
+  }
+//   funcion caliing 
+let result = myFunction("anil"); //anil is a actual argument 
+console.log(result);
+
+// new type ES6 function 
+ let myFunctionZ=(name)=>{
+    console.log("helo " + name + "jiiii");
+
+ }
+ myFunctionZ("deepak ");
